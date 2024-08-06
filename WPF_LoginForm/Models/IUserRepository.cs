01d68086765ae1b83ad7744ac24a0cb56109e475
DESKTOP-VFDI2V8\Mustafa <malhoura@hs-mittweida.de>
2024-08-07 00:26:22 +0200

    firdt

---
+++ WPF_LoginForm/Models/IUserRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_LoginForm.Model;
+
+namespace WPF_LoginForm.Models
+{
+    public interface IUserRepository
+    {
+        bool Authenticateuser(NetworkCredential credential);
+        void Add(UserModel userModel);
+        void Edit(UserModel userModel);
+        void Remove(int id);
+        UserModel GetById(int id);
+        UserModel GetUsername(string username);
+        IEnumerable<UserModel> GetByAll();
+        bool AuthenticateUser(NetworkCredential networkCredential);
+        object GetByUsername(string name);
+        object GetByUsername(IPrincipal currentPrincipal);
+    }
+}
+
+
+
+
---
+++ WPF_LoginForm/Repositories/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WPF_LoginForm.Repositories
+{
+     public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+        public RepositoryBase()
+        {
+            _connectionString = "Server=DESKTOP-VFDI2V8\\MSSQLSERVER01; Database=Task_Mng; Integrated Security=true";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
+ 
